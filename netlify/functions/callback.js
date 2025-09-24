6fba37949a51925343bc43b1d8e6ba0e1abf9196
dafid palevi <43629045+dafidpalevi1@users.noreply.github.com>
2025-09-25 02:53:43 +0700

    Add files via upload

--- netlify/functions/callback.js
+++ netlify/functions/callback.js
@@ -2,7 +2,7 @@
 exports.handler = async function(event, context) {
     return {
       statusCode: 200,
-      body: "tiktok-developers-site-verification=Dsus9rEf4pkNiESPTWeBH04dsz5QuBaQ",
+      body: "tiktok-developers-site-verification=ctyhcf9a2X4VzlE4iTItrJ7q1HS7C5sF",
     };
   };
   
